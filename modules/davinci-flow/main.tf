48c7789a2e06cd67b00c27da6e8ba4d8597124dd
Terry Sigle <terrysigle@pingidentity.com>
2023-09-07 13:07:00 -0500

    changing return from flows

--- modules/davinci-flow/main.tf
+++ modules/davinci-flow/main.tf
@@ -33,8 +33,8 @@ resource "davinci_flow" "demoflow" {
 }
 
 output "flows" {
-  value = [{
+  value = {
     id   = resource.davinci_flow.demoflow.id
     name = resource.davinci_flow.demoflow.name
-  }]
+  }
 }
