6eae341b1179e4077caa885c8b752b25d28ce722
Terry Sigle <terrysigle@pingidentity.com>
2023-09-07 12:36:03 -0500

    Adding davinci-flow-file

---
+++ modules/davinci-flow-file/main.tf
@@ -0,0 +1,36 @@
+
+
+resource "davinci_flow" "demoflow" {
+  environment_id = var.demo_environment_id
+  flow_json      = file(var.flow_file)
+  deploy         = true
+
+  # Look into parsing flow, looking for connectors, constructs, subflows
+
+  dynamic "connection_link" {
+    for_each = var.connections
+    content {
+      name = var.all_conns.connections[index(var.all_conns.names, connection_link.value)].name
+      id   = var.all_conns.connections[index(var.all_conns.names, connection_link.value)].id
+    }
+  }
+
+  dynamic "subflow_link" {
+    for_each = var.subflows
+    content {
+      name = subflow_link.name
+      id   = subflow_link.id
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+output "flows" {
+  value = [{
+    id   = resource.davinci_flow.demoflow.id
+    name = resource.davinci_flow.demoflow.name
+  }]
+}
